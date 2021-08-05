dd89267aa5277e216675599f0116762cbe2bc74d
guilherme070812 <54514054+Guilherme070812@users.noreply.github.com>
2021-08-04 21:38:11 -0300

    create route question

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({
-    hello: "Hello world",
-  });
+const questions = [];
+
+app.get("/questions", (req, res) => {
+  return res.json(questions);
+});
+
+app.post("/questions", (req, res) => {
+  const question = req.body;
+  questions.push(question);
+  return res.json({ ...question, id: uuid() });
 });
 
 app.listen(process.env.PORT || 3333);
